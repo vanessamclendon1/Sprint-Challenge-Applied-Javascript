1d605587af839fa2799de71d0e05bf6636736c6a
Vanessa McLendon <vanessamclendon@Vanessas-MacBook-Pro-2.local>
2019-03-08 12:31:29 -0500

    added class tab and tabCard

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,13 +2,14 @@ class TabLink {
   constructor(tabElement){
     // assign this.tabElement to the tabElement DOM reference
     // this.tabElement;
+    this.tabElement = tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
     // this.tabData = ; 
-    
+    this.tabData = this.tabElement.dataset.tab;
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
-    /* <- Delete this comment block when you work on the if statement
+    
     // Check to see if this.tabData is equal to 'all'
     if(){
       // If `all` is true, select all cards regardless of their data attribute values
@@ -17,7 +18,7 @@ class TabLink {
       // else if `all` is false, only select the cards with matching this.tabData values
       // this.cards = ;
     }
-    /* <- Delete this comment block when you work on the if statement
+   
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
     // this.cards = Array.from(this.cards).map();
@@ -25,6 +26,7 @@ class TabLink {
     // Add a click event that invokes this.selectTab
     // this.tabElement.addEventListener();
   }
+ 
 
   selectTab(){
 
@@ -51,16 +53,18 @@ class TabLink {
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+     this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-    // this.cardElement;
+    this.cardElement.style.display = 'flex';
   }
 
 }
 
-/* START HERE: 
+
+
+/*START HERE: 
 
 - Select all classes named ".tab" and assign that value to the tabs variable
 
@@ -69,4 +73,7 @@ class TabCard {
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
 
 */
-let tabs = document.querySelectorAll();
+
+let tabs = document.querySelectorAll('.tab').forEach(tab => new TabLink(tab));
+  new TabLink(tab)	
+  console.log(tabs);
